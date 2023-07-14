372dbc956588f370e146bd326ea351a3a8314f18
Nneoma <nneoma.amy@gmail.com>
2023-07-14 16:23:16 +0000

    reworking maze traversal

--- src/maze.ml
+++ src/maze.ml
@@ -54,9 +54,12 @@ let rec traverse_maze
   Stack.push current_path current_cell;
   (* check if we hit the exit - base case and we win! *)
   (* let ref x =[] !x (this makes this a reference list) x := [5; 5;5] *)
-  if Char.equal
-       maze_matrix.(Coord.get_r current_cell).(Coord.get_c current_cell)
-       'E'
+  let found_exit =
+    Char.equal
+      maze_matrix.(Coord.get_r current_cell).(Coord.get_c current_cell)
+      'E'
+  in
+  if found_exit
   then
     ()
     (* current path is mutable so we will already have the solve_command
@@ -65,23 +68,44 @@ let rec traverse_maze
     (* if we eventually hit base case it will return a current_ath*)
 
     (* change to fold in order to stop the endless popping *)
-    List.iter directions ~f:(fun dir ->
-      print_s [%message "" (has_visited : Coord.Hash_set.t)];
-      print_s [%message "" (current_path : Coord.t Base.Stack.t)];
-      let new_cell = Coord.add current_cell dir in
-      if in_bounds new_cell
-         && open_path new_cell
-         && not (Hash_set.mem has_visited new_cell)
-      then (
-        Hash_set.add has_visited new_cell;
-        traverse_maze maze_matrix new_cell has_visited current_path));
+    let traverse_tracker = 0 in
+    let traverse_tracker =
+      List.fold
+        directions
+        ~init:(traverse_tracker, found_exit)
+        ~f:(fun (traverse_tracker, found_exit) dir ->
+        [%message
+          ""
+            (current_path : Coord.t Stack.t)
+            (traverse_tracker : int)
+            (dir : Coord.t)]
+        |> Sexp.to_string_hum
+        |> print_endline;
+        print_endline "-------------------------";
+        if not found_exit
+        then (
+          let new_cell = Coord.add current_cell dir in
+          if in_bounds new_cell
+             && open_path new_cell
+             && not (Hash_set.mem has_visited new_cell)
+          then (
+            let traverse_tracker = traverse_tracker + 1 in
+            Hash_set.add has_visited new_cell;
+            traverse_maze maze_matrix new_cell has_visited current_path;
+            (* its not stopping after we find the solution *)
+            traverse_tracker, found_exit)
+          else traverse_tracker, found_exit)
+        else traverse_tracker, found_exit)
+    in
     (* if we reach the end of directions all of it sucked and we must
        backtrack *)
 
     (* i didnt really back track, i just kept popping off *)
-    let pop_cell = Stack.pop_exn current_path in
-    ignore pop_cell;
-    ())
+    if traverse_tracker >= List.length directions
+    then (
+      let pop_cell = Stack.pop_exn current_path in
+      ignore pop_cell;
+      ()))
 ;;
 
 (* takes in a maze represented as a string and returns the solution set as a
@@ -125,4 +149,19 @@ let solve_command =
 
 let command =
   Command.group ~summary:"maze commands" [ "solve", solve_command ]
+;;
+
+let dfs root =
+  let rec f node path =
+    if is_end
+         node (* doesnt have current node so put it in before returning *)
+    then Some path
+    else
+      (* make sure get children ONLY returns valid cells (in bounds, not
+         visited [things in the path so we dont go backwards], and not a
+         wall)*)
+      List.map (get_children node) ~f:(fun child -> f child (node :: path))
+      |> List.find_map ~f:(fun path -> path)
+  in
+  f root []
 ;;
